bd83383a95da59c0f852869c6f9a8b436413edf0
60629194 <jim940502@gmail.com>
2024-11-25 22:28:47 +0800

    debuging

--- cube100.asm
+++ cube100.asm
@@ -32,9 +32,10 @@
 	rz dw 1, 0, 0, 0, 1, 0, 0, 0, 1
     Pp dw 320, 0, 240 ;front of center of the screen
 	scr dw 500 ;y coordinate
+	EXTERNDEF sin1:WORD 
 .code
-#include trig.inc
-#include math.inc
+include trig.inc
+include math.inc
 main PROC
     ;catch input
     mov ax, @data
@@ -47,9 +48,9 @@ ipt:
     je topspin
     cmp ah, 50h ;down
     je backspin
-    cmp al, 4bh ;left
+    cmp ah, 4bh ;left
     je clockwise
-    cmp al, 4dh ;right
+    cmp ah, 4dh ;right
     je counterclockwise
     cmp al, 27 ;esc
     je exit
@@ -89,523 +90,555 @@ cal:
 ;refresh rx, rz
 	mov ax, thetax
 	call cos
-	mov rx[4], ax
 	mov rx[8], ax
+	mov rx[16], ax
 	mov rz[0], ax
-	mov rz[4], ax
+	mov rz[8], ax
 	mov ax, thetax
 	call sin
-	mov rx[7], ax
-	mov rz[3], ax
+	mov rx[14], ax
+	mov rz[6], ax
 	neg ax
-	mov rx[5], ax
-	mov rz[1], ax
+	mov rx[10], ax
+	mov rz[2], ax
 
 ;Qn=Qn-Qc
 	mov ax, Qc[0]
 	sub word ptr Q1[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q1[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q1[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q1[4], ax
 	
 	mov ax, Qc[0]
 	sub word ptr Q2[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q2[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q2[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q2[4], ax
 	
 	mov ax, Qc[0]
 	sub word ptr Q3[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q3[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q3[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q3[4], ax
 	
 	mov ax, Qc[0]
 	sub word ptr Q4[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q4[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q4[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q4[4], ax
 	
 	mov ax, Qc[0]
 	sub word ptr Q5[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q5[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q5[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q5[4], ax
 	
 	mov ax, Qc[0]
 	sub word ptr Q6[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q6[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q6[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q6[4], ax
 	
 	mov ax, Qc[0]
 	sub word ptr Q7[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q7[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q7[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q7[4], ax
 	
 	mov ax, Qc[0]
 	sub word ptr Q8[0], ax
-	mov ax, Qc[1]
-	sub word ptr Q8[1], ax
 	mov ax, Qc[2]
 	sub word ptr Q8[2], ax
+	mov ax, Qc[4]
+	sub word ptr Q8[4], ax
 
 ;rxQ1
-	mov ax, rx[4]
-	mov dx, Q1[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q1[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q1[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q1[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q1[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q1[4]
+	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q1[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q1[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q1[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q1[4], ax
 ;rzQ1
 	mov ax, rz[0]
 	mov dx, Q1[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q1[1]
+	mov ax, rz[2]
+	mov dx, Q1[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q1[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q1[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q1[1]
+	mov ax, rz[8]
+	mov dx, Q1[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q1[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q1[2], ax
 
 ;rxQ2
-	mov ax, rx[4]
-	mov dx, Q2[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q2[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q2[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q2[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q2[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q2[4]
+	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q2[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q2[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q2[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q2[4], ax
 ;rzQ2
 	mov ax, rz[0]
 	mov dx, Q2[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q2[1]
+	mov ax, rz[2]
+	mov dx, Q2[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q2[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q2[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q2[1]
+	mov ax, rz[8]
+	mov dx, Q2[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q2[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q2[2], ax
 
 ;rxQ3
-	mov ax, rx[4]
-	mov dx, Q3[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q3[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q3[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q3[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q3[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q3[4]
+	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q3[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q3[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q3[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q3[4], ax
 ;rzQ3
 	mov ax, rz[0]
 	mov dx, Q3[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q3[1]
+	mov ax, rz[2]
+	mov dx, Q3[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q3[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q3[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q3[1]
+	mov ax, rz[8]
+	mov dx, Q3[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q3[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q3[2], ax
 
 ;rxQ4
-	mov ax, rx[4]
-	mov dx, Q4[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q4[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q4[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q4[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q4[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q4[4]
+	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q4[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q4[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q4[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q4[4], ax
 ;rzQ4
 	mov ax, rz[0]
 	mov dx, Q4[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q4[1]
+	mov ax, rz[2]
+	mov dx, Q4[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q4[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q4[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q4[1]
+	mov ax, rz[8]
+	mov dx, Q4[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q4[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q4[2], ax
 	
 ;rxQ5
-	mov ax, rx[4]
-	mov dx, Q5[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q5[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q5[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q5[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q5[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q5[4]
+	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q5[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q5[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q5[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q5[4], ax
 ;rzQ5
 	mov ax, rz[0]
 	mov dx, Q5[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q5[1]
+	mov ax, rz[2]
+	mov dx, Q5[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q5[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q5[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q5[1]
+	mov ax, rz[8]
+	mov dx, Q5[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q5[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q5[2], ax
 
 ;rxQ6
-	mov ax, rx[4]
-	mov dx, Q6[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q6[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q6[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q6[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q6[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q6[4]
+	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q6[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q6[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q6[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q6[4], ax
 ;rzQ6
 	mov ax, rz[0]
 	mov dx, Q6[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q6[1]
+	mov ax, rz[2]
+	mov dx, Q6[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q6[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q6[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q6[1]
+	mov ax, rz[8]
+	mov dx, Q6[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q6[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q6[2], ax
 
 ;rxQ7
-	mov ax, rx[4]
-	mov dx, Q7[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q7[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q7[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q7[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q7[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q7[4]
+	imul dx
 	call EXadd
-	idiv 5000
-	mov Q7[2], ax	
+	mov bx, 5000 
+	idiv bx
+	mov Q7[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q7[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q7[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q7[4], ax	
 ;rzQ7
 	mov ax, rz[0]
 	mov dx, Q7[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q7[1]
+	mov ax, rz[2]
+	mov dx, Q7[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q7[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q7[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q7[1]
+	mov ax, rz[8]
+	mov dx, Q7[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q7[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q7[2], ax
 
 ;rxQ8
-	mov ax, rx[4]
-	mov dx, Q8[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
-	mov ax, rx[5]
-	mov dx, Q8[2]
-	imul dx
-	call EXadd
-	idiv 5000
-	mov Q8[1], ax
-	;
-	mov ax, rx[7]
-	mov dx, Q8[1]
-	imul dx
-	mov bx, dx
-	mov cx, ax
 	mov ax, rx[8]
 	mov dx, Q8[2]
 	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[10]
+	mov dx, Q8[4]
+	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q8[2], ax
+	;
+	mov ax, rx[14]
+	mov dx, Q8[2]
+	imul dx
+	mov bx, dx
+	mov cx, ax
+	mov ax, rx[16]
+	mov dx, Q8[4]
+	imul dx
+	call EXadd
+	mov bx, 5000 
+	idiv bx
+	mov Q8[4], ax
 ;rzQ8
 	mov ax, rz[0]
 	mov dx, Q8[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[1]
-	mov dx, Q8[1]
+	mov ax, rz[2]
+	mov dx, Q8[2]
 	imul dx
 	call EXadd
-	idiv 5000
+	mov bx, 5000 
+	idiv bx
 	mov Q8[0], ax
 	;
-	mov ax, rz[3]
+	mov ax, rz[6]
 	mov dx, Q8[0]
 	imul dx
 	mov bx, dx
 	mov cx, ax
-	mov ax, rz[4]
-	mov dx, Q8[1]
+	mov ax, rz[8]
+	mov dx, Q8[2]
 	imul dx
 	call EXadd
-	idiv 5000
-	mov Q8[1], ax
+	mov bx, 5000 
+	idiv bx
+	mov Q8[2], ax
 	
 ;Qn=Qn+Qc
 	mov ax, Qc[0]
 	add word ptr Q1[0], ax
-	mov ax, Qc[1]
-	add word ptr Q1[1], ax
 	mov ax, Qc[2]
 	add word ptr Q1[2], ax
+	mov ax, Qc[4]
+	add word ptr Q1[4], ax
 	
 	mov ax, Qc[0]
 	add word ptr Q2[0], ax
-	mov ax, Qc[1]
-	add word ptr Q2[1], ax
 	mov ax, Qc[2]
 	add word ptr Q2[2], ax
+	mov ax, Qc[4]
+	add word ptr Q2[4], ax
 	
 	mov ax, Qc[0]
 	add word ptr Q3[0], ax
-	mov ax, Qc[1]
-	add word ptr Q3[1], ax
 	mov ax, Qc[2]
 	add word ptr Q3[2], ax
+	mov ax, Qc[4]
+	add word ptr Q3[4], ax
 	
 	mov ax, Qc[0]
 	add word ptr Q4[0], ax
-	mov ax, Qc[1]
-	add word ptr Q4[1], ax
 	mov ax, Qc[2]
 	add word ptr Q4[2], ax
+	mov ax, Qc[4]
+	add word ptr Q4[4], ax
 	
 	mov ax, Qc[0]
 	add word ptr Q5[0], ax
-	mov ax, Qc[1]
-	add word ptr Q5[1], ax
 	mov ax, Qc[2]
 	add word ptr Q5[2], ax
+	mov ax, Qc[4]
+	add word ptr Q5[4], ax
 	
 	mov ax, Qc[0]
 	add word ptr Q6[0], ax
-	mov ax, Qc[1]
-	add word ptr Q6[1], ax
 	mov ax, Qc[2]
 	add word ptr Q6[2], ax
+	mov ax, Qc[4]
+	add word ptr Q6[4], ax
 	
 	mov ax, Qc[0]
 	add word ptr Q7[0], ax
-	mov ax, Qc[1]
-	add word ptr Q7[1], ax
 	mov ax, Qc[2]
 	add word ptr Q7[2], ax
+	mov ax, Qc[4]
+	add word ptr Q7[4], ax
 	
 	mov ax, Qc[0]
 	add word ptr Q8[0], ax
-	mov ax, Qc[1]
-	add word ptr Q8[1], ax
 	mov ax, Qc[2]
 	add word ptr Q8[2], ax
+	mov ax, Qc[4]
+	add word ptr Q8[4], ax
 
 
 
