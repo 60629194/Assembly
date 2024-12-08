f04e4b8cdeb2efec55969f27960224a6ba792ba3
the-biKing <joshhsieh1129@gmail.com>
2024-12-08 11:31:59 +0800

    vmem is faster

--- DOSmasm615/xmain.asm
+++ DOSmasm615/xmain.asm
@@ -1,22 +1,38 @@
 .model small
 .stack
 .data
-    EXTERNDEF sin1:WORD
+	color db 2
 .code
+include vmem.inc
+main proc
+	mov ax, @data
+	mov ds, ax
+	
+	mov ax, 12h
+	int 10h
+	
+	mov cx, 0
+	mov dx, 0
+	
+y:	call vmem
+	
+	inc dx
+	cmp dx, 480
+	jae x
+	jmp y
 
-include trig.inc
-
-main PROC
-    mov ax, @data
-    mov ds, ax
-    mov bx, 0
-    mov ax, 233
-    call sin
-    mov ax, 233
-    call cos
-
-    mov ax, 4C00h
-    int 21h
-
-main ENDP
+x:	inc cx
+	mov dx, 0
+	cmp cx, 640
+	jae exit
+	jmp y
+	
+exit:
+	mov ax, 0003h
+	int 10h
+	
+	mov ax, 4c00h
+	int 21h
+		
+main endp
 end main
